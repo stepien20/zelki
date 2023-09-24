4fdea9c89a6f2d6d7ad22fd93588c1c34135aabe
stepien20 <102522088+stepien20@users.noreply.github.com>
2023-09-24 14:36:00 +0200

    js

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-butOrder=document.querySelector(`#butord`)
-console.log(butOrder)
+
+const butOrder=document.querySelector(`#butord`)
 butOrder.addEventListener(`click`,function(evt){
     evt.preventDefault()
-    shape=document.querySelector(`#shape`).value
-    colorR=document.querySelector(`#colorR`).value
-    colorG=document.querySelector(`#colorG`).value
-    colorB=document.querySelector(`#colorB`).value
-    if(colorR>255||colorG>255||colorB>255){
-        alert(`Podaj RGB w zakresie 0-255 !!!`)
+    const shape=document.querySelector(`#shape`).value
+    const colorR=document.querySelector(`#colorR`).value
+    const colorG=document.querySelector(`#colorG`).value
+    const colorB=document.querySelector(`#colorB`).value
+    if(colorR>255||colorG>255||colorB>255||colorR<0||colorG<0||colorB<0){
+        alert(`Podaj RGB w zakresie 0-255 !!!`);
+        return 0;
+    }
+    const tabshape=document.querySelectorAll(`li[class="shape"]`)
+    let chosenShape=''
+    for(let i=1;i<=tabshape.length;i++){
+        if (i==shape){
+            chosenShape=tabshape[i-1].outerText
+            break;
+        }
+        chosenShape='inny kształt'
+        break;
     }
     display=document.querySelector(`#display`)
-})
+    display.innerHTML=`Zamówiłeś żelka: ${chosenShape}`
+    butcol=document.querySelector(`#butcol`)
+    butcol.style.backgroundColor=`rgb(${colorR},${colorG},${colorB})`
+}
+)
